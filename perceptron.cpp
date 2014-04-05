203a2479fb7b7074d3924ef357f926eb96677fae
Amandeep Gill <agill@cwolf.cs.sonoma.edu>
2014-04-05 14:21:29 -0700

    added perceptron data type with input and update functions

---
+++ perceptron.cpp
@@ -0,0 +1,45 @@
+#include "perceptron.h"
+
+Perceptron * makePerceptron(int len) {
+    Perceptron * p = new Perceptron;
+    p->len = len;
+    p->b = 0.0;
+    p->w = new float[len];
+    p->a = 0.1;
+
+    int i;
+    for (i = 0; i < len; i++) {
+        p->w[i] = 0.0;
+    }
+
+    return p;
+}
+
+float y(Perceptron * p, int * i) {
+    float dot = 0.0;
+
+    int j;
+    for (j = 0; j < p->len; j++) {
+        dot += p->w[j] * i[j];
+    }
+
+    dot += p->b;
+
+    return dot > 0.0;
+}
+
+bool f(Perceptron * p, int * i) {
+    return y(p, i) > 0.0;
+}
+
+void update(Perceptron * p, int * i, bool out) {
+    int d = out ? 1 : 0;
+    float yi = y(p, i);
+
+    int j;
+    for (j = 0; j < p->len; j++) {
+        p->w[j] += (p->a) * (i[j]) * (d - yi);
+    }
+
+    p->b += (p->a) * (d - yi);
+}
---
+++ perceptron.h
@@ -0,0 +1,24 @@
+#ifndef PERCEPTRON_H
+#define PERCEPTRON_H
+
+typedef struct {
+    // the learning rate of the perceptron
+    float a;
+    // the length of the weight vector
+    int len;
+    // the bias factor
+    float b;
+    // the weight vector
+    float * w;
+} Perceptron;
+
+// create a new perceptron with a randomized weight vector of length len
+Perceptron * makePerceptron(int len);
+// returns the value of the dot product of the weights and input vectors
+float y(Perceptron * p, int * i);
+// returns true if the dot product of the input and weight vectors is greater than 0
+bool f(Perceptron * p, int * i);
+// update the weight values of the perceptron with the given input and the desired output
+void update(Perceptron * p, int * i, bool out);
+
+#endif // PERCEPTRON_H
