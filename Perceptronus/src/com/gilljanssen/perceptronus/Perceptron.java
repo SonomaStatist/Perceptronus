8e07920d929dd065eee99472c80b497249ae874a
Kyle Janssen <janssen@seawolf.sonoma.edu>
2014-04-23 14:03:28 -0700

    Pushing up the new IDEA project in the Perceptronus folder.

---
+++ Perceptronus/src/com/gilljanssen/perceptronus/Perceptron.java
@@ -0,0 +1,13 @@
+package com.gilljanssen.perceptronus;
+
+public class Perceptron {
+
+    // the learning rate of the perceptron
+    float a;
+    // the length of the weight vector
+    int len;
+    // the bias factor
+    float b;
+    // the weight vector
+    float[] w;
+}
