0b22edbdcfe4d60599ba38e1783c91d3787b33e2
Kyle Janssen <janssen@seawolf.sonoma.edu>
2014-04-23 14:39:22 -0700

    Moved the data parsing into its own file, so all main has to do is ask for the data.

--- Perceptronus/src/com/gilljanssen/perceptronus/Main.java
+++ Perceptronus/src/com/gilljanssen/perceptronus/Main.java
@@ -1,53 +1,16 @@
 package com.gilljanssen.perceptronus;
 
-import au.com.bytecode.opencsv.CSVReader;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<String[]> TTTData;
-        List<String[]> CancerData;
+        DataParser dataParser = new DataParser();
+        List<String[]> TTTData = dataParser.TTTData;
+        List<String[]> CancerData = dataParser.CancerData;
+        List<String[]> TestTTTData = dataParser.TestTTTData;
+        List<String[]> TestCancerData = dataParser.TestCancerData;
 
-        try {
-            CSVReader reader = new CSVReader(new FileReader("data/tictactoe.data"));
-            TTTData = reader.readAll();
-
-            reader = new CSVReader(new FileReader("data/cancer.data"));
-            CancerData = reader.readAll();
-
-            for (int i = 0; i < TTTData.size(); i++) {
-
-                System.out.print("TTT " + (i + 1) + ": ");
-
-                for (int j = 0; j < TTTData.get(i).length; j++) {
-                    System.out.print(TTTData.get(i)[j] + " ");
-                }
-
-                System.out.print("\n");
-            }
-
-            for (int i = 0; i < CancerData.size(); i++) {
-
-                System.out.print("Cancer " + (i + 1) + ": ");
-
-                for (int j = 0; j < CancerData.get(i).length; j++) {
-                    System.out.print(CancerData.get(i)[j] + " ");
-                }
-
-                System.out.print("\n");
-            }
-
-
-
-
-
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {}
     }
 }
